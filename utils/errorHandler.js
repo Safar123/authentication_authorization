4f602142734cf11926ba98d02e31366e924e3c4c
Safar123 <azajsbd111@gmail.com>
2022-01-06 16:21:44 +0545

    diabling object distructure for error

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,4 +1,4 @@
-
+const AppError = require('./appError');
 const responseDevError =(err, res)=>{
 res.status(err.statusCode).json({
     status:err.status,
@@ -25,17 +25,29 @@ const responseProdError = (err, res)=>{
 
 }
 
+const handleCastErrorDB = err=>{
+    const message = `Invalid ${err.path} : ${err.value}`;
+    return new AppError(message, 400)
+
+}
+
 module.exports = (err,req,res,next)=>{
 
     err.statusCode =err.statusCode || 500;
     err.status = err.status || 'error';
 
-    if(process.env.NODE_ENV==='development'){
+    if(process.env.NODE_ENV==="development"){
        
         responseDevError(err,res)
     }
 
-    else if(process.env.NODE_ENV==='production'){
+    else if(process.env.NODE_ENV==="production"){
+
+        //let error ={...err}
+        //console.log(error);
+        if(err.name ==="CastError") {
+            err= handleCastErrorDB(err)
+        } 
 
         responseProdError(err, res)
 
