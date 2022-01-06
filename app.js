ca330a1d8ecc115789f317cee7268cd2a9cbb398
Safar123 <azajsbd111@gmail.com>
2022-01-06 14:41:02 +0545

    route handler initiation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const app = express();
+
+const userRoute = require('./routes/userRoutes')
+
+app.use(express.json())
+app.use('/api/v1/users', userRoute);
 
 
 module.exports = app;
