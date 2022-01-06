9ffac44d9f330fc6d09bf149f448ba3f83216d8b
Safar123 <azajsbd111@gmail.com>
2022-01-06 15:03:36 +0545

    General error handling middleware

---
+++ utils/errorHandler.js
@@ -0,0 +1,43 @@
+
+const responseDevError =(err, res)=>{
+res.status(err.statusCode).json({
+    status:err.status,
+    message:err.message,
+    error:err,
+    stack:err.stack
+})
+}
+
+const responseProdError = (err, res)=>{
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status:err.status,
+            message:err.message
+        })
+    }
+
+    else{
+        res.status(500).json({
+            status:'error',
+            message:'Something went wrong !!!!!'
+        })
+    }
+
+}
+
+module.exports = (err,req,res,next)=>{
+
+    err.statusCode =err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    if(process.env.NODE_ENV==='development'){
+       
+        responseDevError(err,res)
+    }
+
+    else if(process.env.NODE_ENV==='production'){
+
+        responseProdError(err, res)
+
+    }
+}
