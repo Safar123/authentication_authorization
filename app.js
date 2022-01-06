2156fe368109b0d42919e818ec0a3087cc45b40c
Safar123 <azajsbd111@gmail.com>
2022-01-06 16:20:41 +0545

    fixing position of error handler

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
+const errorHandler = require('./utils/errorHandler');
 const userRoute = require('./routes/userRoutes')
 
 app.use(express.json())
+
 app.use('/api/v1/users', userRoute);
 
-
+app.use(errorHandler)
 module.exports = app;
